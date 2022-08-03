79eac33d9f977370492dd47ee8e69c58c24862f5
ayushma-jain <ayushmaj46@gmail.com>
2022-08-03 14:43:49 +0530

    Firebase setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log("Checking Firebase");
+
 root.render(
   
   <React.StrictMode>
